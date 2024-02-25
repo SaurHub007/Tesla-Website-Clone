e0635e655513c8a860897fdb0b390b0096af290c
SaurHub007 <161135159+SaurHub007@users.noreply.github.com>
2024-02-25 21:59:17 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -10,3 +10,9 @@ document.getElementById('menuBtn').onclick = e =>{
     e.preventDefault();
     openMenu(); 
 }
+document.querySelector('aside button.close').onclick = e =>{
+    closeMenu();
+}
+document.querySelector('.backdrop').onclick = e =>{
+    closeMenu();
+}
