66a9e35e402dd5cda0bf2cb5e0a8a62f239aaa90
SaurHub007 <161135159+SaurHub007@users.noreply.github.com>
2024-02-25 21:58:43 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -2,3 +2,7 @@ const openMenu = () => {
     document.querySelector('.backdrop').className='backdrop active';
     document.querySelector('aside').className='active';    
 }
+const closeMenu =() =>{
+    document.querySelector('.backdrop').className = 'backdrop';
+    document.querySelector('aside').className='';
+}
